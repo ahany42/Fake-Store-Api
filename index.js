85246d2a33e7d74ea71cfa7e376c52ea0efe9e4d
Ahany42 <ahanyfathy42@gmail.com>
2024-07-12 01:49:22 +0300

    fetching data from api

---
+++ index.js
@@ -0,0 +1,26 @@
+for(let i =1;i<=5;i++){
+    let data=fetch(`https://fakestoreapi.com/products/${i}`)
+    .then(res=>res.json())
+    .then(json=>{
+        let newDiv=document.createElement("div");
+        newDiv.innerHTML=`
+        <div>
+        <div>
+        <h5>${json.title}</h5>
+        <h6>${json.category}</h6>
+        <p>${json.description}</p>
+        <div>
+        <h6>${json.price}</h6>
+        <h6>${json.rating}</h6>
+        </div>
+        <img src=${json.image}>
+        </div>
+        </div>
+     
+        `;
+        document.getElementById("container").appendChild(newDiv);
+        console.log(json)
+    })
+    .catch(error => console.error('Error:', error));
+
+}
